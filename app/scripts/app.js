ac89fa407654dc3999bc4289066b0d6e43763ab8
Geoff Pavey <geoff.pavey@gmail.com>
2015-06-16 17:11:30 -0700

    home page first blush

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,28 +24,54 @@ angular
 .controller('MainCtrl', function($scope) {
   $scope.opacityState = 'closed';
 })
+.controller('mfbController',function($scope){
+  $scope.buttons = [{
+      label: 'invite a friend',
+      icon: 'ion-paper-airplane',
+      state: 'invite'
+    },{
+      label: 'Complete an Atta',
+      icon: 'ion-checkmark',
+      state: 'complete'
+    },{
+      label: 'Create an Atta',
+      icon: 'ion-edit',
+      state: 'create'
+  }];
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
   $stateProvider
+
      .state('invite', {
-       url         : '/invite',
-       templateUrl : '../views/invite.html',
-       controller  : 'inviteController'
+       url          : '/invite',
+       templateUrl  : '../views/invite.html',
+       controller   : 'inviteController'
      })
      .state('complete', {
-       url         : '/complete',
-       templateUrl : '../views/complete.html',
-       controller  : 'completeController'
+       url          : '/complete',
+       templateUrl  : '../views/complete.html',
+       controller   : 'completeController'
      })
      .state('create', {
-       url         : '/create',
-       templateUrl : '../views/create.html',
-       controller  : 'createController'
+       url          : '/create',
+       templateUrl  : '../views/create.html',
+       controller   : 'createController'
      })
+    .state('edit_profile', {
+      url           : '/edit_profile',
+      templateUrl   : '/..views/edit_profile.html',
+      controller    : 'editProfileController'
+    })
     .state('profile', {
       url           : '/profile',
       templateUrl   : '../views/profile.html',
       controller    : 'profileController'
+    })
+    .state('/',{
+      url           : '/',
+      templateUrl   : '../views/home.html',
+      // controller    : 'homeCtrl'
     });
 });
--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -2,16 +2,76 @@
 
 /**
  * @ngdoc function
- * @name attasApp.controller:MainCtrl
+ * @name attasApp.controller:HomeCtrl
  * @description
- * # MainCtrl
+ * # HomeCtrl
  * Controller of the attasApp
  */
 angular.module('attasApp')
-  .controller('homeController', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+  .controller('homeCtrl', function($scope){
+    $scope.menuState = 'closed';
+    $scope.buttons = [{
+      label: 'invite a friend',
+      icon: 'ion-paper-airplane',
+      state: 'invite'
+    },{
+      label: 'Complete an Atta',
+      icon: 'ion-checkmark',
+      state: 'complete'
+    },{
+      label: 'Create an Atta',
+      icon: 'ion-edit',
+      state: 'create'
+    }];
+  })
+
+  .controller('similarAttaCtrl', function($scope){
+    var imagePath = '';
+    $scope.messages = [
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      },
+      {
+        face : imagePath,
+        what: 'Volunteer at pet shelter',
+      }
     ];
   });
